fbbae828ab5925518ac0d8cff5ec7c570cb76b74
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-08-08 18:46:59 +0530

    done

---
+++ main.js
@@ -0,0 +1,31 @@
+$('#btn1').click(function(){
+$('#step1').hide();
+$('#step2').show();
+});
+
+$('#btn2').click(function(){
+    $('#step2').hide();
+    $('#step3').show();
+    });
+
+    $('#btn3').click(function(){
+        $('#step3').hide();
+        $('#step4').show();
+        });
+    
+        $('#pbtn2').click(function(){
+            $('#step2').hide();
+            $('#step1').show();
+            });
+        
+            $('#pbtn3').click(function(){
+                $('#step3').hide();
+                $('#step2').show();
+                });
+            
+                $('#pbtn4').click(function(){
+                    $('#step4').hide();
+                    $('#step3').show();
+                    });
+                
+                   
